3c72c97e38dc945ed53282c884cb39e30485f9af
Thomas Alexander Woolff <153089340+TAWoolff1508@users.noreply.github.com>
2024-09-28 18:53:21 +0200

    Add collaborations and works pages + formelements components

---
+++ src/app/works/page.tsx
@@ -0,0 +1,12 @@
+import css from './works.module.css'
+
+const Works: React.FC = () => {
+  return ( 
+    <section className={css.works}>
+      <h1>Works</h1>
+      <p>This is the works page</p>
+    </section>
+  )
+}
+
+export default Works
