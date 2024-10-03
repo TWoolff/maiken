d76411013954424e1b74a15d76d335567ab3d02f
Thomas Alexander Woolff <153089340+TAWoolff1508@users.noreply.github.com>
2024-10-03 16:48:42 +0200

    type fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,31 +2,31 @@
 import { useEffect, useState } from 'react'
 import { useAppContext } from '@/services/context'
 import { getPage } from '@/services/contentful'
+import { HomeData } from '@/types/types' 
 import Video from '@/components/video/video'
 
 const Home: React.FC = () => {
-  const {state, dispatch} = useAppContext()
-  //@ts-ignore
-  const [homeData, setHomeData] = useState<any | null>(null)
+  const { state, dispatch } = useAppContext()
+  const [homeData, setHomeData] = useState<HomeData | null>(null)
 
   useEffect(() => {
-		if (homeData) return;
-		const fetchData = async () => {
-			try {
-				const data = await getPage('home');
-				setHomeData(JSON.parse(JSON.stringify(data.items[0].fields)))
-			} catch (error) {
-				console.error('Error fetching home data:', error);
-			}
-		}
-		fetchData()
-	}, [homeData])
+    if (homeData) return
+    const fetchData = async () => {
+      try {
+        const data = await getPage('home')
+        setHomeData(JSON.parse(JSON.stringify(data.items[0].fields)) as HomeData)
+      } catch (error) {
+        console.error('Error fetching home data:', error)
+      }
+    }
+    fetchData()
+  }, [homeData])
 
   useEffect(() => {
-		if (homeData && state.data) {
-			dispatch({ type: 'SET_STATE', payload: { hasLoaded: true } })
-		}
-	}, [homeData, state.data, dispatch])
+    if (homeData && state.data) {
+      dispatch({ type: 'SET_STATE', payload: { hasLoaded: true } })
+    }
+  }, [homeData, state.data, dispatch])
 
   //console.log('homeData:', homeData)
 
--- src/types/types.ts
+++ src/types/types.ts
@@ -1,26 +1,74 @@
-import { Dispatch } from 'react'
+import { Dispatch } from 'react';
 
 export type AppContextType = {
-    state: State
-    dispatch: Dispatch<Action>
-}
+	state: State;
+	dispatch: Dispatch<Action>;
+};
 
 export type Action = {
-    type: 'SET_STATE' | 'TOGGLE_LANGUAGE'
-    payload?: Partial<State>  | { id: string } | { key: string, value: string } | null
-}
+	type: 'SET_STATE' | 'TOGGLE_LANGUAGE';
+	payload?: Partial<State> | { id: string } | { key: string; value: string } | null;
+};
 
 // export type DataState = {
 //     data?: string
 // } | null | undefined | any
 
-export type ErrorState = string | null
+export type ErrorState = string | null;
 
 export type State = {
-    error: ErrorState | null
-    data: null //  | DataState
-    hasLoaded: boolean
-    language: 'da' | 'en'
-}
+	error: ErrorState | null;
+	data: null; //  | DataState
+	hasLoaded: boolean;
+	language: 'da' | 'en';
+};
 
+type RichTextNode = {
+	nodeType: string;
+	content: Array<{
+		nodeType: string;
+		value?: string;
+		content?: Array<RichTextNode>;
+	}>;
+};
 
+type ProjectEntry = {
+	metadata: {
+		tags: any[];
+	};
+	sys: {
+		id: string;
+		type: string;
+		createdAt: string;
+		updatedAt: string;
+		contentType: {
+			sys: {
+				id: string;
+				type: string;
+				linkType: string;
+			};
+		};
+		locale: string;
+	};
+	fields: {
+		title_dk: string;
+		title_eng: string;
+		slug: string;
+		description_dk: RichTextNode;
+		description_eng: RichTextNode;
+	};
+};
+
+export type HomeData = {
+	title: string;
+	slug: string;
+	projects: {
+		sys: {
+			id: string;
+			type: string;
+		};
+		fields: {
+			project: ProjectEntry[];
+		};
+	};
+};
