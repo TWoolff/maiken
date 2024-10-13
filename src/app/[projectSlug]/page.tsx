d167728fcbeabd47c23a8ab7ca4677f16d8951c0
Thomas Alexander Woolff <153089340+TAWoolff1508@users.noreply.github.com>
2024-10-13 08:10:57 +0200

    background image on project page

--- src/app/[projectSlug]/page.tsx
+++ src/app/[projectSlug]/page.tsx
@@ -1,14 +1,16 @@
 import { findEntryBySlug } from '@/services/contentful';
+import css from './project.module.css';
 
 const ProjectPage = async ({ params }: { params: { projectSlug: string } }) => {
   const project = await findEntryBySlug(params.projectSlug);
+  const mainImgUrl = project?.fields?.mainImg?.['en-US']?.fields?.file?.['en-US']?.url;
 
   if (!project) {
     return <div>Project not found</div>;
   }
 
   return (
-    <section className='grid space'>
+    <section className={`${css.project} grid space`} style={{ backgroundImage: mainImgUrl ? `url(${mainImgUrl})` : 'none'}}>
       <h1>{project.fields.title['en-US']}</h1>
     </section>
   );
