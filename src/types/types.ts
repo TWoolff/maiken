07c156339c5ca8c1003d8eb7746e39a6e7a8707e
Thomas Alexander Woolff <153089340+TAWoolff1508@users.noreply.github.com>
2024-11-07 11:49:13 +0100

    types clean up

--- src/types/types.ts
+++ src/types/types.ts
@@ -1,18 +1,14 @@
 import { Dispatch } from 'react'
-import { EntrySkeletonType } from 'contentful'
 
 export type AppContextType = {
 	state: State
 	dispatch: Dispatch<Action>
 }
 
-export type Action =
-  | { type: 'SET_STATE'; payload: Partial<State> }
-  | { type: 'TOGGLE_LANGUAGE'; payload: { language: 'da-DK' | 'en-US' } }
-  | { type: 'SET_NAV'; payload: string }
+export type Action = { type: 'SET_STATE'; payload: Partial<State> } | { type: 'TOGGLE_LANGUAGE'; payload: { language: 'da-DK' | 'en-US' } } | { type: 'SET_NAV'; payload: string }
 
 export type DataState = {
-    data?: PageData[]
+	data?: PageData[]
 }
 
 export type ErrorState = string | null
@@ -22,251 +18,231 @@ export type State = {
 	data: null | DataState
 	hasLoaded: boolean
 	language: 'da-DK' | 'en-US'
-  currentNav: string
+	currentNav: string
 }
 
 export type ContentfulTag = {
-  sys: {
-    id: string
-    type: string
-    linkType: string 
-  }
-  name: string
+	sys: {
+		id: string
+		type: string
+		linkType: string
+	}
+	name: string
 }
 
 export type LocalizedFields<T> = {
-  [locale: string]: T
+	[locale: string]: T
 }
 
-
-export type TextContentEntry = {
-  sys: {
-    id: string
-    type: string
-  }
-  fields: {
-    id: LocalizedFields<string>
-    text: LocalizedFields<RichTextDocument>
-  }
+export interface BaseEntry {
+	metadata: Metadata
+	sys: FullSys
+	fields: {
+		id: LocalizedFields<string>
+	}
 }
 
-export interface ProjectEntry extends EntrySkeletonType {
-  metadata: {
-    tags: ContentfulTag[]
-  }
-  sys: {
-    id: string
-    type: string
-    createdAt: string
-    updatedAt: string
-    contentType: {
-      sys: {
-        id: string
-        type: string
-        linkType: string
-      }
-    }
-    locale: string
-  }
-  fields: {
-    title: LocalizedFields<string>
-    year: LocalizedFields<number>
-    slug: LocalizedFields<string>
-    content: LocalizedFields<RichTextDocument[]>
-    mainImg?: LocalizedFields<{ fields: { file: { 'en-US': { url: string } } } }>
-  }
+export interface TextContentEntry extends BaseEntry {
+	fields: {
+		id: LocalizedFields<string>
+		text: LocalizedFields<RichTextDocument>
+	}
+}
+
+export interface ProjectEntry extends BaseEntry {
+	fields: {
+		id: LocalizedFields<string>
+		title: LocalizedFields<string>
+		year: LocalizedFields<number>
+		slug: LocalizedFields<string>
+		content: LocalizedFields<RichTextDocument[]>
+		mainImg?: LocalizedFields<{ fields: { file: { 'en-US': { url: string } } } }>
+	}
 }
 
 export type ProjectContent = {
-  sys: {
-    id: string
-    type: string
-  }
-  fields: {
-    id: LocalizedFields<string>
-    project: LocalizedFields<ProjectEntry[]>
-  }
+	sys: {
+		id: string
+		type: string
+	}
+	fields: {
+		id: LocalizedFields<string>
+		project: LocalizedFields<ProjectEntry[]>
+	}
 }
 
 export type ContentItem = ProjectContent | TextContentEntry
 
 export type InitData = {
-  title: LocalizedFields<string>
-  slug: LocalizedFields<string>
-  content: LocalizedFields<ContentItem[]>
+	title: LocalizedFields<string>
+	slug: LocalizedFields<string>
+	content: LocalizedFields<ContentItem[]>
 }
 
-export type ContentfulDocument = {
-  content: { content: { value: string }[] }[]
-} | undefined
+export type ContentfulDocument =
+	| {
+			content: { content: { value: string }[] }[]
+	  }
+	| undefined
 
 export interface Metadata {
-  tags: ContentfulTag[];
+	tags: Array<{
+		sys: ContentTypeSys
+		name: string
+	}>
 }
 
-export interface Sys {
-  space: {
-    sys: {
-      type: string;
-      linkType: string;
-      id: string;
-    };
-  };
-  id: string;
-  type: string;
-  createdAt: string;
-  updatedAt: string;
-  environment: {
-    sys: {
-      id: string;
-      type: string;
-      linkType: string;
-    };
-  };
-  publishedVersion: number;
-  revision: number;
-  contentType: {
-    sys: {
-      type: string;
-      linkType: string;
-      id: string;
-    };
-  };
+export interface BaseSys {
+	id: string
+	type: string
+}
+
+export interface ContentTypeSys extends BaseSys {
+	linkType: string
+}
+
+export interface FullSys extends BaseSys {
+	createdAt: string
+	updatedAt: string
+	contentType: {
+		sys: ContentTypeSys
+	}
+	locale?: string
 }
 
 export interface LocalizedField<T> {
-  [locale: string]: T;
+	[locale: string]: T
 }
 
 export interface PageFields {
-  title: LocalizedField<string>;
-  slug: LocalizedField<string>;
-  content: LocalizedField<ContentEntry[]>;
+	title: LocalizedField<string>
+	slug: LocalizedField<string>
+	content: LocalizedField<ContentEntry[]>
 }
 
 export interface ContentEntry {
-  metadata: Metadata;
-  sys: Sys;
-  fields: {
-    id: LocalizedField<string>;
-    text: LocalizedField<RichTextDocument>;
-  };
+	metadata: Metadata
+	sys: FullSys
+	fields: {
+		id: LocalizedField<string>
+		text: LocalizedField<RichTextDocument>
+	}
 }
 
 export interface RichTextDocument {
-  nodeType: string;
-  data: NodeData;
-  content: Array<{
-    data: NodeData;
-    content: Array<{
-      data: NodeData;
-      marks: Array<{ type: string }>;
-      value: string;
-      nodeType: string;
-    }>;
-    nodeType: string;
-  }>;
+	nodeType: string
+	data: NodeData
+	content: Array<{
+		data: NodeData
+		content: Array<{
+			data: NodeData
+			marks: Array<{ type: string }>
+			value: string
+			nodeType: string
+		}>
+		nodeType: string
+	}>
 }
 
 export interface PageData {
-  metadata: Metadata;
-  sys: Sys;
-  fields: PageFields;
+	metadata: Metadata
+	sys: FullSys
+	fields: PageFields
 }
 
 export interface RichTextContent {
-  nodeType: string;
-  data: NodeData;
-  content?: RichTextContent[];
-  value?: string;
-  marks?: Array<{ type: string }>;
+	nodeType: string
+	data: NodeData
+	content?: RichTextContent[]
+	value?: string
+	marks?: Array<{ type: string }>
 }
 
 export interface ContactFields {
-  id: LocalizedField<string>;
-  text: LocalizedFields<RichTextDocument>;
+	id: LocalizedField<string>
+	text: LocalizedFields<RichTextDocument>
 }
 
 export interface ContactEntry {
-  metadata: Metadata;
-  sys: Sys;
-  fields: ContactFields;
+	metadata: Metadata
+	sys: FullSys
+	fields: ContactFields
 }
 
 export type NodeData = {
-	uri?: string;
+	uri?: string
 }
 
 export interface ImageAsset {
-  fields: {
-    title: LocalizedFields<string>
-    description?: LocalizedFields<string>
-    file: LocalizedFields<{
-      url: string
-      details: {
-        size: number
-        image: {
-          width: number
-          height: number
-        }
-      }
-      fileName: string
-      contentType: string
-    }>
-  }
+	fields: {
+		title: LocalizedFields<string>
+		description?: LocalizedFields<string>
+		file: LocalizedFields<{
+			url: string
+			details: {
+				size: number
+				image: {
+					width: number
+					height: number
+				}
+			}
+			fileName: string
+			contentType: string
+		}>
+	}
 }
 
 export interface ImageEntry {
-  metadata: Metadata
-  sys: Sys
-  fields: {
-    id: LocalizedFields<string>
-    image: LocalizedFields<ImageAsset>
-  }
+	metadata: Metadata
+	sys: FullSys
+	fields: {
+		id: LocalizedFields<string>
+		image: LocalizedFields<ImageAsset>
+	}
 }
 
 export interface VideoEntry {
-  metadata: Metadata
-  sys: Sys
-  fields: {
-    id: LocalizedFields<string>
-    video: LocalizedFields<{
-      fields: {
-        file: LocalizedFields<{
-          url: string
-          contentType: string
-        }>
-      }
-    }>
-  }
+	metadata: Metadata
+	sys: FullSys
+	fields: {
+		id: LocalizedFields<string>
+		video: LocalizedFields<{
+			fields: {
+				file: LocalizedFields<{
+					url: string
+					contentType: string
+				}>
+			}
+		}>
+	}
 }
 
 export interface ImageDoubleEntry {
-  fields: {
-    imageLeft: {
-      'en-US': {
-        fields: {
-          title: { 'en-US': string };
-          file: { 'en-US': { url: string } }
-        }
-      }
-    };
-    imageRight: {
-      'en-US': {
-        fields: {
-          title: { 'en-US': string };
-          file: { 'en-US': { url: string } }
-        }
-      }
-    };
-    spacing: { 'en-US': string };
-  };
+	fields: {
+		imageLeft: {
+			'en-US': {
+				fields: {
+					title: { 'en-US': string }
+					file: { 'en-US': { url: string } }
+				}
+			}
+		}
+		imageRight: {
+			'en-US': {
+				fields: {
+					title: { 'en-US': string }
+					file: { 'en-US': { url: string } }
+				}
+			}
+		}
+		spacing: { 'en-US': string }
+	}
 }
 
 export interface ContentFields {
-  file: {
-    'en-US': {
-      url: string
-    }
-  }
+	file: {
+		'en-US': {
+			url: string
+		}
+	}
 }
